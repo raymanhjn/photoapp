9abe7aed3d8dc8960e83e65bc7a05609b8aefec2
raymanhjn <hejn0605@hotmail.com>
2017-09-23 11:09:53 -0400

    functions implemented

--- src/pages/photolist/photolist.ts
+++ src/pages/photolist/photolist.ts
@@ -4,10 +4,6 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 import { Http, Headers, RequestOptions } from '@angular/http';
 
 
-// import { GvisionProvider } from '../../providers/gvision/gvision';
-
-
-
 @Component({
   selector: 'page-photolist',
   templateUrl: 'photolist.html',
@@ -23,14 +19,11 @@ export class PhotolistPage {
   synth:any;
   voices:any;
   text: string;
-  videoDom:any;
-  canvasDom:any;
 
   constructor(
       public navCtrl: NavController,
       public navParams: NavParams,
       private camera: Camera,
-      private elementRef : ElementRef,
       public http: Http,
     ) {
   }
@@ -41,19 +34,16 @@ export class PhotolistPage {
     this.synth = window.speechSynthesis;
     this.voices=[];
 
-    this.videoDom = this.videoEle.nativeElement;
-    this.canvasDom = this.canvasEle.nativeElement;
-
-    // if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    //     // Not adding `{ audio: true }` since we only want video now
-    //     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-    //         this.videoDom.src = window.URL.createObjectURL(stream);
-    //         this.videoDom.play();
-    //     });
-    // }
-
+    let video =  this.videoEle.nativeElement;
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+        });
+    }
   }
 
+  //for the native device
   takePhoto() {
     const options: CameraOptions = {
       quality: 100,
@@ -64,16 +54,15 @@ export class PhotolistPage {
 
     this.camera.getPicture(options).then((imageData) => {
       this.base64Image = 'data:image/jpeg;base64,' + imageData;
-      this.imageUrl = imageData;
       this.photo = this.base64Image;
+      this.getData(imageData);
     }, (err) => {
       console.log(err);
     });
   }
 
-  getData() {
+  getData(imageData) {
     let headers = new Headers({'Content-Type' : 'application/json'});
-
     let options = new RequestOptions({ headers: headers });
     let API_KEY = "AIzaSyDRgu_6xNGoVzR81dLP2LD2Pl4si3c0MVg";
     let url = 'https://vision.googleapis.com/v1/images:annotate?key='+API_KEY;
@@ -86,11 +75,7 @@ export class PhotolistPage {
             }
           ],
           "image": {
-              "content":this.imageUrl
-              // "source":{
-              //   "imageUri":
-              //     "gs://photoapp_images/demo-image.jpg"
-              // }
+              "content":imageData
           }
         }
       ]
@@ -113,14 +98,18 @@ export class PhotolistPage {
     window.speechSynthesis.speak(speakContent);
   }
 
+
+  //for web
   snapPhoto() {
     //show on canvas dom
-    let context = this.canvasDom.getContext('2d');
-    context.drawImage(this.videoDom, 0,0,640,480);
+    let context = this.canvasEle.nativeElement.getContext('2d');
+    let video =  this.videoEle.nativeElement;
+    context.drawImage(video, 0,0,640,480);
 
     //convert to base64 decode
-    let dataURL = this.canvasDom.toDataURL("image/png");
+    let dataURL = this.canvasEle.nativeElement.toDataURL("image/png");
     let base64 = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+    // console.log(base64);
 
     // set to Google Vision API and get data back
     let headers = new Headers({'Content-Type' : 'application/json'});
